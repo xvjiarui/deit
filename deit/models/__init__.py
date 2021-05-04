2e8f2b918d4860675d38273387024b5cad46ba22
Jiarui XU <xvjiarui0826@gmail.com>
2021-05-03 17:13:09 -0700

    add recurrent mvit s

--- deit/models/__init__.py
+++ deit/models/__init__.py
@@ -2,3 +2,4 @@ from .builder import build_model
 from .vit import VisionTransformer, RecurrentVisionTransformer
 from .gct import GlobalContextTransformer
 from .mvit import MultiscaleVisionTransformer
+from .rmvit import RecurrentMultiscaleVisionTransformer
